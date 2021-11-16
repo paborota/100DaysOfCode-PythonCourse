ea9241b10cc902438251377c0720bb8f6e265d0e
dasFufu <paul.borota@outlook.com>
2021-11-16 02:06:17 -0600

    Day 49 - Job App Automation Update
    Small change to the way that the Apply button is found. Instead of looking and then handling the error if it's not there, it waits till it's visible. If it still is not found, program terminates immediately.

--- Day49-JobApplicationAutomation/linkedin_interface.py
+++ Day49-JobApplicationAutomation/linkedin_interface.py
@@ -62,50 +62,47 @@ class LinkedInInterface:
         for job in job_list:
             job.click()
 
-            has_been_evaluated = False
-            while not has_been_evaluated:
+            try:
+                # make sure we can find the apply button
+                apply_button = WebDriverWait(self.driver, 5).until(
+                    ec.visibility_of_element_located((By.CSS_SELECTOR, "div.jobs-apply-button--top-card")))
+            except:
+                # if somehow the wait for apply button fails, just return. something must have happened.
+                return
+            else:
                 # check for easy apply, if no easy apply, just save the job listing. If easy apply, apply.
-                try:
-                    apply_button = self.driver.find_element(By.CSS_SELECTOR, "div.jobs-apply-button--top-card")
-                except NoSuchElementException:
-                    # this will trigger if the webpage hasn't loaded the job information yet.
-                    # sleep for a sec and then reiterate to check again
-                    time.sleep(1)
-                    pass
+                if apply_button.find_element(By.CSS_SELECTOR, "span.artdeco-button__text").text == "Easy Apply":
+                    apply_button.click()
+
+                    phone_number_input = self.driver.find_element(By.CSS_SELECTOR, "input[name*='phoneNumber']")
+                    phone_number_input.send_keys(PHONE_NUMBER)
+
+                    # --- uncomment for submit functionality ---
+                    # submit_button = driver.find_element(By.CSS_SELECTOR, "button[aria-label='Submit application']")
+                    # submit_button.click()
+                    # ------------------------------------------
+
+                    # --- comment to turn off auto closing the submit window ---
+                    x_button = self.driver.find_element(By.CSS_SELECTOR, "button[aria-label='Dismiss']")
+                    x_button.click()
+
+                    confirm_discard_button = self.driver.find_element(By.CSS_SELECTOR,
+                                                                      "button[data-control-name='discard_application_confirm_btn']")
+                    confirm_discard_button.click()
+                    # ----------------------------------------------------------
                 else:
-                    if apply_button.find_element(By.CSS_SELECTOR, "span.artdeco-button__text").text == "Easy Apply":
-                        apply_button.click()
+                    save_button = self.driver.find_element(By.CSS_SELECTOR, "button.jobs-save-button")
+                    if "Saved" not in save_button.text:
+                        # if this evals to true, this means that the job listing is not saved
+                        save_button.click()
 
-                        phone_number_input = self.driver.find_element(By.CSS_SELECTOR, "input[name*='phoneNumber']")
-                        phone_number_input.send_keys(PHONE_NUMBER)
-
-                        # uncomment for submit functionality
-                        # submit_button = driver.find_element(By.CSS_SELECTOR, "button[aria-label='Submit application']")
-                        # submit_button.click()
-                        # ----------------------------------
-
-
-                        # comment to turn off auto closing the submit window
-                        x_button = self.driver.find_element(By.CSS_SELECTOR, "button[aria-label='Dismiss']")
-                        x_button.click()
-
-                        confirm_discard_button = self.driver.find_element(By.CSS_SELECTOR, "button[data-control-name='discard_application_confirm_btn']")
-                        confirm_discard_button.click()
-                        # --------------------------------------------------
-                    else:
-                        save_button = self.driver.find_element(By.CSS_SELECTOR, "button.jobs-save-button")
-                        if "Saved" not in save_button.text:
-                            # if this evals to true, this means that the job listing is not saved
-                            save_button.click()
-
-                            try:
-                                # see if there is a notification popup, dismiss if there
-                                popup_dismiss_button = WebDriverWait(self.driver, 5).until(
-                                    ec.visibility_of_element_located((By.CSS_SELECTOR, "button.artdeco-toast-item__dismiss")))
-                            except:
-                                # if any possible errors popup, just ignore all
-                                pass
-                            else:
-                                popup_dismiss_button.click()
-                    # break loop
-                    has_been_evaluated = True
+                        try:
+                            # see if there is a notification popup, dismiss if there
+                            popup_dismiss_button = WebDriverWait(self.driver, 5).until(
+                                ec.visibility_of_element_located(
+                                    (By.CSS_SELECTOR, "button.artdeco-toast-item__dismiss")))
+                        except:
+                            # any possible errors, just ignore all
+                            pass
+                        else:
+                            popup_dismiss_button.click()
