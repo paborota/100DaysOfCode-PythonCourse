8006300d2a50489df950abf7b192735485b89ecc
dasFufu <paul.borota@outlook.com>
2021-12-12 00:22:13 -0600

    Added "Add.html" Functionality
    Finished up the add.html's functionality to append data entered to the CSV file.

--- Starting+Files+-+coffee-and-wifi/main.py
+++ Starting+Files+-+coffee-and-wifi/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
@@ -43,6 +43,16 @@ def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
         print("True")
+        with open('cafe-data.csv', 'a', encoding="utf8") as csv_file:
+            data_to_append = f"\n{form.cafe.data}," +\
+                             f"{form.location.data}," +\
+                             f"{form.open_time.data}," +\
+                             f"{form.closing_time.data}," +\
+                             f"{form.coffee_rating.data}," +\
+                             f"{form.wifi_rating.data}," +\
+                             f"{form.power_availability.data}"
+            csv_file.write(data_to_append)
+            return redirect('/cafes')
     # Exercise:
     # Make the form write a new row into cafe-data.csv
     # with   if form.validate_on_submit()
@@ -51,7 +61,7 @@ def add_cafe():
 
 @app.route('/cafes')
 def cafes():
-    with open('cafe-data.csv', newline='') as csv_file:
+    with open('cafe-data.csv', newline='', encoding="utf8") as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = []
         for row in csv_data:
