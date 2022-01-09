fab27b765a4f6e37e55a82d2b4d968036c2c1454
paborota <paul.borota@outlook.com>
2022-01-08 19:58:54 -0600

    Day 68 Update 2
    Added hashing to password authentication.
    
    Added security to page "resources" that redirects user back to the login page and alerts them what happened.

--- Day 68 Authentication with Flask/main.py
+++ Day 68 Authentication with Flask/main.py
@@ -1,4 +1,4 @@
-
+import flask
 from flask import Flask, flash, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -12,6 +12,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 # #CREATE TABLE IN DB
 class User(UserMixin, db.Model):
@@ -21,12 +24,24 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(1000))
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.query(User).get(user_id)
+
+
 # Line below only required once, when creating DB.
 # db.create_all()
 
 
+def verify_user_logged_in():
+    return current_user.is_authenticated
+
+
 @app.route('/')
 def home():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('secrets'))
 
     return render_template("index.html")
 
@@ -36,10 +51,16 @@ def register():
 
     if request.method == 'POST':
 
+        hashed_password = generate_password_hash(
+            password=request.form.get('password'),
+            method="pbkdf2:sha256",
+            salt_length=8
+        )
+
         new_user = User(
             name=request.form.get('name').title(),
             email=request.form.get('email'),
-            password=request.form.get('password')
+            password=hashed_password
         )
 
         db.session.add(new_user)
@@ -59,19 +80,35 @@ def login():
 
         user = db.session.query(User).filter_by(email=request.form.get('email')).first()
         if user is not None:
+            # The user was found in the DB
+            # - Verify password entered matches password for user.
+            if verify_password(user, request.form.get('password')):
+                # Password matched
+                # - Login the user.
 
-            if user.password == request.form.get('password'):
+                login_user(user)
 
-                return render_template('secrets.html', user=user)
+                # flash("Logged in successfully.")
+
+                return redirect(url_for('secrets'))
 
             else:
-
+                # Passwords did not match
+                # - Alert that the login attempt failed.
+                # - Then allow the website to load the login page again.
                 login_failed()
         else:
-
+            # The email entered could not be found in the DB
+            # - Alert that the login attempt failed.
+            # - Then allow the website to load the login page again.
             login_failed()
 
     return render_template("login.html")
+
+
+def verify_password(user, password):
+
+    return check_password_hash(user.password, password)
 
 
 def login_failed():
@@ -82,19 +119,38 @@ def login_failed():
 @app.route('/secrets')
 def secrets():
 
+    if not verify_user_logged_in():
+        tried_access_without_auth()
+        return redirect(url_for('login'))
+
     return render_template("secrets.html")
 
 
 @app.route('/logout')
 def logout():
 
-    pass
+    if not verify_user_logged_in():
+        flash('You need to be logged in to log out.')
+        return redirect(url_for('login'))
+
+    logout_user()
+    flash('Logged out successfully.')
+
+    return redirect(url_for('login'))
 
 
 @app.route('/download/<filename>', methods=['GET'])
 def download(filename):
 
+    if not verify_user_logged_in():
+        tried_access_without_auth()
+        return redirect(url_for('login'))
+
     return send_from_directory(f"{app.static_folder}/files", filename=filename, as_attachment=True)
+
+
+def tried_access_without_auth():
+    flash('You need to be logged in to access that resource.')
 
 
 if __name__ == "__main__":
